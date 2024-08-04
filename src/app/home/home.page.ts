81df182c91ed5895175c4152cc6dc58edda985e2
nfekah08 <nfekah.odnan@gmail.com>
2024-08-04 14:04:57 +0800

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+ height:number = 170
+ weight:number = 70 
+ bmi:number = 0
+ message:string = ""
+
   constructor() {}
 
+calculateBmi(){
+  this.bmi = Math.round(this.weight / Math.pow(this.height / 100,2))
+
+  if(this.bmi < 18.5){
+    this.message = "You are underweight"
+
+  }
+  else if(this.bmi <25){
+    this.message = "You are normal"
+
+  }
+  else if(this.bmi < 30){
+    this.message = "You are overweight"
+
+  }
+  else if(this.bmi <35){
+    this.message = "You are obese"
+
+  }
+  else {
+    this.message = "You are extremely obese"
+  }
+
+  }
+
+
+  
 }
+
+
